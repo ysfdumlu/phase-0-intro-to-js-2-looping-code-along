ea77f55e8fc97804a2cb79503fd86b75bd581e91
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-05 04:45:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array,event){
+    const mesaj = []
+    for (let i = 0; i < array.length; i++) {
+        mesaj.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return mesaj
+}
+function countDown(number){
+    let i =number;
+    while (i>-1) {
+        console.log(i)
+        i--;
+    }
+}
